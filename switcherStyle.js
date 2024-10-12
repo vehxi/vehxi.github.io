5a3bdfaf82ed6346a8956f6de571f78aa7cab5ef
Alexey Tarakanov <vehxi@MacBook-Air.local>
2024-10-13 02:49:21 +0300

    fix fix fix

--- switcherStyle.js
+++ switcherStyle.js
@@ -1,23 +1,26 @@
 const toggleSwitch = document.getElementById('theme-toggle');
-const body = document.body;
 
-const currentTheme = localStorage.getItem('theme');
-
-if (currentTheme === 'dark') {
-  document.documentElement.classList.add('dark-theme');
-  toggleSwitch.checked = true;
-} else {
-  document.documentElement.classList.add('light-theme');
-}
-
-toggleSwitch.addEventListener('change', function() {
-  if (toggleSwitch.checked) {
-    document.documentElement.classList.remove('light-theme');
+if (typeof(Storage) !== "undefined") {
+  const currentTheme = localStorage.getItem('theme');
+  
+  if (currentTheme === 'dark') {
     document.documentElement.classList.add('dark-theme');
-    localStorage.setItem('theme', 'dark');
+    toggleSwitch.checked = true;
   } else {
-    document.documentElement.classList.remove('dark-theme');
     document.documentElement.classList.add('light-theme');
-    localStorage.setItem('theme', 'light');
   }
-});
+  
+  toggleSwitch.addEventListener('change', function() {
+    if (toggleSwitch.checked) {
+      document.documentElement.classList.remove('light-theme');
+      document.documentElement.classList.add('dark-theme');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      document.documentElement.classList.remove('dark-theme');
+      document.documentElement.classList.add('light-theme');
+      localStorage.setItem('theme', 'light');
+    }
+  });
+} else {
+  console.warn("localStorage не поддерживается в этом браузере.");
+}
