403cba15eb866258abaed6b845b5e9598002b422
vehxi <vehxi.dev@gmail.com>
2025-09-17 09:18:30 +0300

    🚀 Launch v1.1

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const animatedElements = document.querySelectorAll('.fade-in-up');
-    animatedElements.forEach(el => {
-        el.classList.add('visible');
-    });
+document.addEventListener("DOMContentLoaded", () => {
+  const animatedElements = document.querySelectorAll(".fade-in-up");
+  animatedElements.forEach((el) => {
+    el.classList.add("visible");
+  });
 
-    try {
-        feather.replace();
-    } catch (e) {
-        console.error('Feather Icons script not loaded', e);
+  try {
+    lucide.createIcons();
+  } catch (e) {
+    console.error("Lucide icons script not loaded", e);
+  }
+
+  const video = document.querySelector(".rounded-video");
+  if (video) {
+    const promise = video.play();
+    if (promise !== undefined) {
+      promise.catch((error) => {
+        console.warn("Autoplay was prevented by the browser.", error);
+      });
     }
-
-
-    const video = document.querySelector('.rounded-video');
-    
-    if (video) {
-        const promise = video.play();
-
-        if (promise !== undefined) {
-            promise.catch(error => {
-                console.warn("Autoplay was prevented by the browser.", error);
-            });
-        }
-    }
-    
+  }
 });
